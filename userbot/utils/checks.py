62610a32041d160a95ca6517147f80a13abdc83a
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-08-22 09:32:55 +0530

    Add files via upload

---
+++ userbot/utils/checks.py
@@ -0,0 +1,23 @@
+from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
+
+from ..funcs.logger import logging
+
+LOGS = logging.getLogger(__name__)
+
+
+# Admin checker by uniborg
+async def is_admin(lionub, chat_id, userid):
+    if not str(chat_id).startswith("-100"):
+        return False
+    try:
+        req_jo = await lionub.get_permissions(chat_id, userid)
+        chat_participant = req_jo.participant
+        if isinstance(
+            chat_participant, (ChannelParticipantCreator, ChannelParticipantAdmin)
+        ):
+            return True
+    except Exception as e:
+        LOGS.info(str(e))
+        return False
+    else:
+        return False
---
+++ userbot/utils/pluginmanager.py
@@ -0,0 +1,102 @@
+import importlib
+import sys
+from pathlib import Path
+
+from userbot import CMD_HELP, LOAD_PLUG
+
+from ..Config import Config
+from ..funcs import LOADED_CMDS, PLG_INFO
+from ..funcs.logger import logging
+from ..funcs.managers import edit_delete, edit_or_reply
+from ..funcs.session import lionub
+from ..helpers.tools import media_type
+from ..helpers.utils import _liontools, _lionutils, _format, install_pip, reply_id
+from .decorators import admin_cmd, sudo_cmd
+
+LOGS = logging.getLogger("LionZ")
+
+
+def load_module(shortname, plugin_path=None):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        path = Path(f"userbot/plugins/{shortname}.py")
+        checkplugins(path)
+        name = "userbot.plugins.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        LOGS.info("Successfully imported " + shortname)
+    else:
+        if plugin_path is None:
+            path = Path(f"userbot/plugins/{shortname}.py")
+            name = f"userbot.plugins.{shortname}"
+        else:
+            path = Path((f"{plugin_path}/{shortname}.py"))
+            name = f"{plugin_path}/{shortname}".replace("/", ".")
+        checkplugins(path)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.bot = lionub
+        mod.LOGS = LOGS
+        mod.Config = Config
+        mod._format = _format
+        mod.tgbot = lionub.tgbot
+        mod.sudo_cmd = sudo_cmd
+        mod.CMD_HELP = CMD_HELP
+        mod.reply_id = reply_id
+        mod.admin_cmd = admin_cmd
+        mod._lionutils = _lionutils
+        mod._liontools = _liontools
+        mod.media_type = media_type
+        mod.edit_delete = edit_delete
+        mod.install_pip = install_pip
+        mod.parse_pre = _format.parse_pre
+        mod.edit_or_reply = edit_or_reply
+        mod.logger = logging.getLogger(shortname)
+        mod.borg = lionub
+        spec.loader.exec_module(mod)
+        # for imports
+        sys.modules["userbot.plugins." + shortname] = mod
+        LOGS.info("Successfully imported " + shortname)
+
+
+def remove_plugin(shortname):
+    try:
+        cmd = []
+        if shortname in PLG_INFO:
+            cmd += PLG_INFO[shortname]
+        else:
+            cmd = [shortname]
+        for cmdname in cmd:
+            if cmdname in LOADED_CMDS:
+                for i in LOADED_CMDS[cmdname]:
+                    lionub.remove_event_handler(i)
+                del LOADED_CMDS[cmdname]
+        return True
+    except Exception as e:
+        LOGS.error(e)
+    try:
+        for i in LOAD_PLUG[shortname]:
+            lionub.remove_event_handler(i)
+        del LOAD_PLUG[shortname]
+    except BaseException:
+        pass
+    try:
+        name = f"userbot.plugins.{shortname}"
+        for i in reversed(range(len(lionub._event_builders))):
+            ev, cb = lionub._event_builders[i]
+            if cb.__module__ == name:
+                del lionub._event_builders[i]
+    except BaseException:
+        raise ValueError
+
+
+def checkplugins(filename):
+    with open(filename, "r") as f:
+        filedata = f.read()
+    filedata = filedata.replace("sendmessage", "send_message")
+    filedata = filedata.replace("sendfile", "send_file")
+    filedata = filedata.replace("editmessage", "edit_message")
+    with open(filename, "w") as f:
+        f.write(filedata)
