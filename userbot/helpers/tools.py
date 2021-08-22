e81dbd555de93cd509f6ea3ee0c44e24cd32b8c6
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-08-22 09:22:16 +0530

    Add files via upload

---
+++ userbot/helpers/tools.py
@@ -0,0 +1,34 @@
+from html_telegraph_poster import TelegraphPoster
+
+
+def media_type(message):
+    if message and message.photo:
+        return "Photo"
+    if message and message.audio:
+        return "Audio"
+    if message and message.voice:
+        return "Voice"
+    if message and message.video_note:
+        return "Round Video"
+    if message and message.gif:
+        return "Gif"
+    if message and message.sticker:
+        return "Sticker"
+    if message and message.video:
+        return "Video"
+    if message and message.document:
+        return "Document"
+    return None
+
+
+async def post_to_telegraph(page_title, html_format_content):
+    post_client = TelegraphPoster(use_api=True)
+    auth_name = "LionUserbot"
+    post_client.create_api_token(auth_name)
+    post_page = post_client.post(
+        title=page_title,
+        author=auth_name,
+        author_url="https://t.me/LionXupdates",
+        text=html_format_content,
+    )
+    return post_page["url"]
