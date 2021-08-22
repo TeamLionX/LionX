923f398cfbff286f8c01741fad933105b087684f
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-08-22 09:29:24 +0530

    Add files via upload

---
+++ userbot/__init__.py
@@ -0,0 +1,79 @@
+import time
+
+import heroku3
+
+from .Config import Config
+from .funcs.logger import logging
+from .funcs.session import lionub
+from .sql_helper.globals import addgvar, delgvar, gvarstatus
+
+__version__ = "1.0.1"
+__license__ = "GNU Affero General Public License v3.0"
+__author__ = "LionUserBot <https://github.com/MdNoor786/Lion-Z>"
+__copyright__ = "Lion-Z Copyright (©️) 2020 - 2021  " + __author__
+
+lionub.version = __version__
+lionub.tgbot.version = __version__
+LOGS = logging.getLogger("Lion-Z")
+bot = lionub
+
+StartTime = time.time()
+lionversion = "1.0.2"
+
+if Config.UPSTREAM_REPO == "lion":
+    UPSTREAM_REPO_URL = "https://github.com/MdNoor786/Lion-Z"
+else:
+    UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
+
+if Config.PRIVATE_GROUP_BOT_API_ID == 0:
+    if gvarstatus("PRIVATE_GROUP_BOT_API_ID") is None:
+        Config.BOTLOG = False
+        Config.BOTLOG_CHATID = "me"
+    else:
+        Config.BOTLOG_CHATID = int(gvarstatus("PRIVATE_GROUP_BOT_API_ID"))
+        Config.PRIVATE_GROUP_BOT_API_ID = int(gvarstatus("PRIVATE_GROUP_BOT_API_ID"))
+        Config.BOTLOG = True
+else:
+    if str(Config.PRIVATE_GROUP_BOT_API_ID)[0] != "-":
+        Config.BOTLOG_CHATID = int("-" + str(Config.PRIVATE_GROUP_BOT_API_ID))
+    else:
+        Config.BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
+    Config.BOTLOG = True
+
+if Config.PM_LOGGER_GROUP_ID == 0:
+    if gvarstatus("PM_LOGGER_GROUP_ID") is None:
+        Config.PM_LOGGER_GROUP_ID = -100
+    else:
+        Config.PM_LOGGER_GROUP_ID = int(gvarstatus("PM_LOGGER_GROUP_ID"))
+elif str(Config.PM_LOGGER_GROUP_ID)[0] != "-":
+    Config.PM_LOGGER_GROUP_ID = int("-" + str(Config.PM_LOGGER_GROUP_ID))
+
+try:
+    if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
+        HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[
+            Config.HEROKU_APP_NAME
+        ]
+    else:
+        HEROKU_APP = None
+except Exception:
+    HEROKU_APP = None
+
+
+# Global Configiables
+COUNT_MSG = 0
+USERS = {}
+COUNT_PM = {}
+LASTMSG = {}
+CMD_HELP = {}
+ISAFK = False
+AFKREASON = None
+CMD_LIST = {}
+SUDO_LIST = {}
+# for later purposes
+INT_PLUG = ""
+LOAD_PLUG = {}
+
+# Variables
+BOTLOG = Config.BOTLOG
+BOTLOG_CHATID = Config.BOTLOG_CHATID
+PM_LOGGER_GROUP_ID = Config.PM_LOGGER_GROUP_ID
---
+++ userbot/__main__.py
@@ -0,0 +1,77 @@
+import sys
+
+import userbot
+from userbot import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
+
+from .Config import Config
+from .funcs.logger import logging
+from .funcs.session import lionub
+from .utils import (
+    add_bot_to_logger_group,
+    ipchange,
+    load_plugins,
+    setup_bot,
+    startupmessage,
+    verifyLoggerGroup,
+)
+
+LOGS = logging.getLogger("Lion-Z")
+
+print(userbot.__copyright__)
+print("Licensed under the terms of the " + userbot.__license__)
+
+cmdhr = Config.COMMAND_HAND_LER
+
+try:
+    LOGS.info("Starting Userbot")
+    lionub.loop.run_until_complete(setup_bot())
+    LOGS.info("TG Bot Startup Completed")
+except Exception as e:
+    LOGS.error(f"{e}")
+    sys.exit()
+
+
+class LionCheck:
+    def __init__(self):
+        self.sucess = True
+
+
+Lioncheck = LionCheck()
+
+
+async def startup_process():
+    check = await ipchange()
+    if check is not None:
+        Lioncheck.sucess = False
+        return
+    await verifyLoggerGroup()
+    await load_plugins("plugins")
+    await load_plugins("assistant")
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+    print("Yay your Lion-Z Userbot is officially working.!!!")
+    print(
+        f"Congratulation, now type {cmdhr}alive to see message if lionub is live\
+        \nIf you need assistance, head to https://t.me/LionXSupport"
+    )
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+    await verifyLoggerGroup()
+    await add_bot_to_logger_group(BOTLOG_CHATID)
+    if PM_LOGGER_GROUP_ID != -100:
+        await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
+    await startupmessage()
+    Lioncheck.sucess = True
+    return
+
+
+lionub.loop.run_until_complete(startup_process())
+
+if len(sys.argv) not in (1, 3, 4):
+    lionub.disconnect()
+elif not Lioncheck.sucess:
+    if HEROKU_APP is not None:
+        HEROKU_APP.restart()
+else:
+    try:
+        lionub.run_until_disconnected()
+    except ConnectionError:
+        pass
