fd1041d3437e8cb7d8936fc72c1c2264e4096621
MdNoor786 <MdNoor786@users.noreply.github.com>
2021-09-15 06:33:51 +0000

    pylint: auto fixes

--- userbot/plugins/gadmin.py
+++ userbot/plugins/gadmin.py
@@ -13,8 +13,8 @@ from telethon.utils import get_display_name
 
 from userbot import lionub
 
-from ..funcs.managers import edit_delete, edit_or_reply
 from ..funcs.devs import DEVLIST
+from ..funcs.managers import edit_delete, edit_or_reply
 from ..helpers.utils import _format
 from ..sql_helper import gban_sql_helper as gban_sql
 from ..sql_helper.mute_sql import is_muted, mute, unmute
