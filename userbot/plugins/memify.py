64caef3ba17300150a00c48a8cc30cf74b34b798
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-10-25 12:30:35 +0530

    Update memify.py

--- userbot/plugins/memify.py
+++ userbot/plugins/memify.py
@@ -13,7 +13,7 @@ from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from userbot import lionub
 
 from ..funcs.managers import edit_delete, edit_or_reply
-from ..helpers import asciiart, cat_meeme, cat_meme, media_type
+from ..helpers import asciiart, lion_meeme, lion_meme, media_type
 from ..helpers.functions import (
     add_frame,
     convert_toimage,
