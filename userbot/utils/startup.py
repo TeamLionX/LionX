3c6f7841e50d27878794520da782adcda43dc1eb
Md Noor <75603153+Mdnoor786@users.noreply.github.com>
2022-01-09 10:13:21 +0530

    Update startup.py

--- userbot/utils/startup.py
+++ userbot/utils/startup.py
@@ -22,7 +22,7 @@ from ..sql_helper.globals import addgvar, delgvar, gvarstatus
 from .pluginmanager import load_module
 from .tools import create_supergroup
 
-LOGS = logging.getLogger("LionX")
+LOGS = logging.getLogger("LionZ")
 cmdhr = Config.COMMAND_HAND_LER
 
 
