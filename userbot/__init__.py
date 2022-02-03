cf85268383bf2b9213ee19ed4085cd899a35e2f0
Md Noor <75603153+Mdnoor786@users.noreply.github.com>
2022-02-03 19:34:49 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -14,7 +14,7 @@ __copyright__ = "Lion-Z Copyright (©️) 2020 - 2021  " + __author__
 
 lionub.version = __version__
 lionub.tgbot.version = __version__
-LOGS = logging.getLogger("LionZ")
+LOGS = logging.getLogger("LionX")
 bot = lionub
 
 StartTime = time.time()
