a04f02ddc1d6d58ed6b9db8a61c93d6e17efc301
Md Noor <75603153+Mdnoor786@users.noreply.github.com>
2022-02-03 12:02:25 +0530

    Update invite.py

--- userbot/plugins/invite.py
+++ userbot/plugins/invite.py
@@ -1,3 +1,7 @@
+import asyncio
+from userbot import catub
+from telethon import functions
+from telethon.errors.rpcerrorlist import PeerFloodError, ChatWriteForbiddenError, ChatAdminRequiredError
 from telethon import functions
 from telethon.errors import (
     ChannelInvalidError,
@@ -53,104 +57,154 @@ async def _(event):
     await edit_or_reply(event, f"`{to_add_users} is/are Invited Successfully`")
 
 
-async def get_chatinfo(event):
-    chat = event.pattern_match.group(1)
-    chat_info = None
-    if chat:
-        try:
-            chat = int(chat)
-        except ValueError:
-            pass
-    if not chat:
-        if event.reply_to_msg_id:
-            replied_msg = await event.get_reply_message()
-            if replied_msg.fwd_from and replied_msg.fwd_from.channel_id is not None:
-                chat = replied_msg.fwd_from.channel_id
-        else:
-            chat = event.chat_id
+async def delete_in(message, time):
+    await asyncio.sleep(time)
+    return await message.delete()
+# X=============Spent Time Anyway=======================X
+
+Scrapping = {} # Well this will be needed to stop scrapping at a moment.
+
+@lionub.lion_cmd(
+    pattern="inviteall (.*)",
+    command=("inviteall", "tools"),
+    info={
+        "header": "To mass add members.",
+        "options": "You can mention limit of members to be added.",
+        "usage": [
+            "{tr}inviteall <chat> <limit>(optional)",
+        ],
+    },
+)
+async def scrapper(event):
+    """Will try to kidnap a whole bunch of people from a chat to a chat i.e user pollination."""
+    if event.is_private:
+        message = await event.edit("Man, do you think I can't differentiate between a channel/group and a personal chat?")
+        return await delete_in(message, 5)
+
     try:
-        chat_info = await event.client(GetFullChatRequest(chat))
-    except BaseException:
-        try:
-            chat_info = await event.client(GetFullChannelRequest(chat))
-        except ChannelInvalidError:
-            await event.reply("`Invalid channel/group`")
-            return None
-        except ChannelPrivateError:
-            await event.reply(
-                "`This is a private channel/group or I am banned from there`"
-            )
-            return None
-        except ChannelPublicGroupNaError:
-            await event.reply("`Channel or supergroup doesn't exist`")
-            return None
-        except (TypeError, ValueError):
-            await event.reply("`Invalid channel/group`")
-            return None
-    return chat_info
+        chat = int((event.pattern_match.group(1)).split(" ")[0])
+    except ValueError:
+        chat = (event.pattern_match.group(1)).split(" ")[0]
 
+    try:
+        max = int((event.pattern_match.group(1)).split(" ")[1])
+        if max==0:
+            message = await event.edit("Zero members?! If you aren't in mood of adding members then why even using me?")
+            return delete_in(message, 5)
+    except IndexError:
+        max = None
+    except ValueError:
+        return await event.edit(max + "is wrong value. An integral value is required.")
 
-def make_mention(user):
-    if user.username:
-        return f"@{user.username}"
-    else:
-        return inline_mention(user)
+    try:
+        chat = await event.client.get_entity(chat)
+    except ValueError:
+        message = await event.edit(f"I don't think `{chat}` is a correct chat. Or maybe you are banned from it." )
+        return await delete_in(message, 8)
 
+    if event.chat_id == chat.id or str(event.chat_id) == ("-100"+f"{chat.id}"):
+        message = await event.edit("You just simply tried to add members from the same group. Like WTF? I guess you need to be added in a mental hospital.")
+        return await delete_in(message, 8)
 
-def inline_mention(user):
-    full_name = user_full_name(user) or "No Name"
-    return f"[{full_name}](tg://user?id={user.id})"
+    try:
+        await event.edit("Getting list of members...")
+        members = (await event.client.get_participants(chat.id)).total
+    except ChatAdminRequiredError:
+        message = await event.edit("I am really sorry, you don't have sufficient administrative privilages required to do that.")
+        return await delete_in(message, 5)
 
+    await event.edit("Starting...")
+    Scrapping.update({event.chat_id: True})
 
-def user_full_name(user):
-    names = [user.first_name, user.last_name]
-    names = [i for i in list(names) if i]
-    full_name = " ".join(names)
-    return full_name
+    # ====================== Constants =====================================
+    warn_surpress = False # Stops a stupid spam that I have created.
+    success = 0 # Counts number of successful attempts to add members.
+    failed = 0 # Counts number of failed attempts to add members.
+    # =====================================================================
+
+    if event.is_group:
+        async for user in event.client.iter_participants(chat.id, limit=max, aggressive=True): # This limit thing isn't blowing anything...!
+            if max == 0:
+                return
+            if event.chat_id not in Scrapping: # Haha, the spell to stop scrapping.
+                return
+
+            try:
+
+                await event.client(functions.messages.AddChatUserRequest(
+                    chat_id=event.chat_id,
+                    user_id=user.id,
+                    fwd_limit=int(members)
+                ))
+                success+=1
+                if max: max-=1
+                await event.edit("Brought " + success + " participant(s)")
+                asyncio.sleep(1)
+
+            except ChatWriteForbiddenError:
+                await event.delete()
+                return await event.respond("You have insufficient rights to invite members here.")
+            except PeerFloodError:
+                if not warn_surpress:
+                    warn_surpress = True
+                    await event.respond("**I am really sorry. You have got Flood-Error from Telegram. Maybe you can't add non-mutual contacts. Please contact** @spambot **for more info.**")
+            except Exception:
+                failed+=1
+
+    if event.is_channel:
+        async for user in event.client.iter_participants(chat.id):
+            if max == 0:
+                return
+            if event.chat_id not in Scrapping: # Haha, read comment on line 39.
+                return
+
+            try:
+                await event.client(functions.channels.InviteToChannelRequest(
+                    channel=event.chat_id,
+                    users=[user.id]
+                ))
+                success+=1
+                if max: max-=1
+                await event.edit("Brought " + success + " participant(s)")
+                asyncio.sleep(1)
+
+            except ChatWriteForbiddenError:
+                await event.delete()
+                return await event.respond("You have insufficient rights to invite members here.")
+            except PeerFloodError:
+                if not warn_surpress:
+                    warn_surpress = True
+                    await event.respond("**I am really sorry. You have got Flood-Error from Telegram. Maybe you can't add non-mutual contacts. Please contact** @spambot **for more info.**")
+            except Exception:
+                failed+=1
+
+    await event.delete()
+    await event.respond(f"**Phew Done**\n__Stats:__\n**Kidnapped** __{success}__ **members. But couldn't kidnap** __{failed}__ **nerds.**")
+
+    try:
+        del Scrapping[event.chat_id]
+    except KeyError:
+        await event.respond("**Wait!! I just got some error. Please restart the bot quickly or reinstall the plugin!!**")
 
 
 @lionub.lion_cmd(
-    pattern=r"allinvite ?(.*)",
-    command=("allinvite", plugin_category),
+    pattern="stopinvite",
+    command=("stopinvite", "tools"),
     info={
-        "header": "Add the given grouplink to the group where u used the command.",
-        "description": "Adds all members",
-        "usage": "{tr}allinvite <grouplink(s)/groupid(s)>",
-        "examples": "{tr} @MissRose_bot",
+        "header": "To stop kidnapping in the current chat.",
+        "usage": [
+            "{tr}stopinvite",
+        ],
     },
 )
-async def get_users(event):
-    sender = await event.get_sender()
-    me = await event.client.get_me()
-    if not sender.id == me.id:
-        rkp = await event.reply("`inviting...`")
+async def kill_scrapper(event):
+    """If you wanna stop this stupid User Pollination, use this."""
+    if event.chat_id not in Scrapping:
+        message = await event.edit("I am not kidnapping in this chat at the moment :D")
+        return await delete_in(message, 5)
     else:
-        rkp = await event.edit("`inviting...`")
-    rk1 = await get_chatinfo(event)
-    chat = await event.get_chat()
-    if event.is_private:
-        return await rkp.edit("`Sorry, Can't add users here`")
-    s = 0
-    f = 0
-    error = "None"
-
-    await rkp.edit("**InvitingStatus**\n\n`Collecting Users.......`")
-    async for user in event.client.iter_participants(rk1.full_chat.id):
         try:
-            if error.startswith("Too"):
-                return await rkp.edit(
-                    f"**Inviting Finished With Error**\n(`May Got Limit Error from telethon Please try agin Later`)\n**Error** : \n`{error}`\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people"
-                )
-            await event.client(
-                functions.channels.InviteToChannelRequest(channel=chat, users=[user.id])
-            )
-            s = s + 1
-            await rkp.edit(
-                f"**Inviting...**\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people\n\n**× LastError:** `{error}`"
-            )
-        except Exception as e:
-            error = str(e)
-            f = f + 1
-    return await rkp.edit(
-        f"**Kidnapping** \n\n• Successfully Invited `{s}` people \n• failed to invite `{f}` people"
-    )
+            del Scrapping[event.chat_id]
+        except KeyError:
+            return await event.edit("Got something wrong. Please reinstall the plugin or restart the bot.")
+        await event.edit("Ok, I will stop this kidnapping here.")
