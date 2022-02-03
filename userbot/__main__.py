7b4d38db934638df10c5cd669521aec4ae681125
Md Noor <75603153+Mdnoor786@users.noreply.github.com>
2022-02-03 19:35:05 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -17,7 +17,7 @@ from .utils import (
     verifyLoggerGroup,
 )
 
-LOGS = logging.getLogger("LionZ")
+LOGS = logging.getLogger("LionX")
 
 print(userbot.__copyright__)
 print("Licensed under the terms of the " + userbot.__license__)
