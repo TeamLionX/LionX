a0a164bcefd382e3c123d883603c2e9cb31ba720
Md Noor <75603153+Mdnoor786@users.noreply.github.com>
2022-02-19 19:51:07 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -69,7 +69,7 @@ class Config(object):
     TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "LionX")
     # for custom thumb image set this with your required thumb telegraoh link
     THUMB_IMAGE = os.environ.get(
-        "THUMB_IMAGE", "https://telegra.ph/file/698a0a9060789f095897c.jpg"
+        "THUMB_IMAGE", "https://telegra.ph/file/4843f54f43db47292f184.jpg"
     )
     # specify NO_LOAD with plugin names for not loading in userbot
     NO_LOAD = [x for x in os.environ.get("NO_LOAD", "").split()]
