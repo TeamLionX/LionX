d990e3ae4923947d28ea24ea680bf2b4437827c8
Md Noor <75603153+Mdnoor786@users.noreply.github.com>
2022-03-18 09:17:06 +0530

    Add files via upload

--- sample_config.py
+++ sample_config.py
@@ -6,11 +6,11 @@ import os
 from typing import Set
 
 from telethon.tl.types import ChatBannedRights
+from validators.url import url
 
 
 class Config(object):
     LOGGER = True
-
     # MUST NEEDED VARS
     # set this value with your name
     ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
@@ -19,7 +19,7 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH") or None
     # Datbase url heroku sets it automatically else get this from elephantsql
     DB_URI = os.environ.get("DATABASE_URL", None)
-    # Get this value by running python3 stringsetup.py or https://repl.it/@SimpleBoy786/generatestringsession
+    # Get this value by running python3 stringsetup.py or https://repl.it/@TeaMlionx/generatestringsession
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     # Telegram BOT Token and bot username from @BotFather
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN") or os.environ.get(
@@ -28,20 +28,20 @@ class Config(object):
     TG_BOT_USERNAME = None
     # get this value from http://www.timezoneconverter.com/cgi-bin/findzone.tzc
     TZ = os.environ.get("TZ", "Asia/Kolkata")
-    # set this with required lion repo link
+    # set this with required lionx repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/TeamLionX/LionX.git"
+        "UPSTREAM_REPO", "https://github.com/TeamLionX/LionX"
     )
-
+    # External plugins repo
+    EXTERNAL_REPO = os.environ.get("EXTERNAL_REPO", None)
+    if EXTERNAL_REPO and not url(EXTERNAL_REPO):
+        EXTERNAL_REPO = "https://github.com/TeamLionX/LionXPlugins"
+    # if you need lionx plugins use the following vars
+    LIONX = bool(os.environ.get("LIONX", False))
     # BASIC and MAIN CONFIG VARS
     # for profile default name
-    AUTONAME = os.environ.get("AUTONAME", None)
     # Set this value with group id of private group(can be found this value by .id)
-    PRIVATE_GROUP_BOT_API_ID = int(
-        os.environ.get("PRIVATE_GROUP_BOT_API_ID")
-        or os.environ.get("PM_LOGGER_GROUP_ID")
-        or 0
-    )
+    PRIVATE_GROUP_BOT_API_ID = int(os.environ.get("PRIVATE_GROUP_BOT_API_ID") or 0)
     # Set this value same as PRIVATE_GROUP_BOT_API_ID if you need pmgaurd
     PRIVATE_GROUP_ID = int(os.environ.get("PRIVATE_GROUP_ID") or 0)
     # Set this value for working of fban/unfban/superfban/superunfban cmd
@@ -58,7 +58,6 @@ class Config(object):
     PM_LOGGER_GROUP_ID = int(
         os.environ.get("PM_LOGGER_GROUP_ID")
         or os.environ.get("PM_LOGGR_BOT_API_ID")
-        or os.environ.get("PRIVATE_GROUP_BOT_API_ID")
         or 0
     )
 
@@ -66,21 +65,13 @@ class Config(object):
     # set this will channel id of your custom plugins
     PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL") or 0)
     # set this value with your required name for telegraph plugin
-    TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "LionX")
+    TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "lionx")
     # for custom thumb image set this with your required thumb telegraoh link
     THUMB_IMAGE = os.environ.get(
-        "THUMB_IMAGE", "https://telegra.ph/file/4843f54f43db47292f184.jpg"
+        "THUMB_IMAGE", "https://telegra.ph/file/aa434c6d0e51a2b1e747a.jpg"
     )
     # specify NO_LOAD with plugin names for not loading in userbot
     NO_LOAD = list(os.environ.get("NO_LOAD", "").split())
-    # for custom pic for .digitalpfp
-    DIGITAL_PIC = os.environ.get("DIGITAL_PIC", None)
-    # your default pic telegraph link
-    DEFAULT_PIC = os.environ.get("DEFAULT_PIC", None)
-    # set this with your default bio
-    DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
-    # set this with your deafult name
-    DEFAULT_NAME = os.environ.get("DEFAULT_NAME", None)
     # specify command handler that should be used for the plugins
     # this should be a valid "regex" pattern
     COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", r".")
@@ -89,12 +80,8 @@ class Config(object):
     TMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY", "downloads")
     # set this with required folder path to act as temparary folder
     TEMP_DIR = os.environ.get("TEMP_DIR", "./temp/")
-    # time to update autoprofile cmds
-    CHANGE_TIME = int(os.environ.get("CHANGE_TIME", 60))
     # SpamWatch, CAS, SpamProtection ban Needed or not
     ANTISPAMBOT_BAN = os.environ.get("ANTISPAMBOT_BAN", False)
-    # is dual logging needed or not true or false
-    DUAL_LOG = os.environ.get("DUAL_LOG", False)
     # progress bar progress
     FINISHED_PROGRESS_STR = os.environ.get("FINISHED_PROGRESS_STR", "▰")
     UNFINISHED_PROGRESS_STR = os.environ.get("UNFINISHED_PROGRESS_STR", "▱")
@@ -118,7 +105,7 @@ class Config(object):
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
     # Get this value from https://free.currencyconverterapi.com/
     CURRENCY_API = os.environ.get("CURRENCY_API", None)
-    # Google Drive plugin https://telegra.ph/G-Drive-guide-for-LionX-01-01
+    # Google Drive plugin https://telegra.ph/G-Drive-guide-for-lionx-01-01
     G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
     G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
     G_DRIVE_FOLDER_ID = os.environ.get("G_DRIVE_FOLDER_ID", None)
@@ -134,6 +121,9 @@ class Config(object):
     LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
     LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME", None)
     LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD", None)
+    # Spotify API for spotify.py // get from here :  https://developer.spotify.com/dashboard/login
+    SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID", None)
+    SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET", None)
     # SpamWatch API you can get it from get api from http://t.me/SpamWatchBot?start=token
     SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
     # can get from https://coffeehouse.intellivoid.net/
@@ -161,15 +151,22 @@ class Config(object):
     GROUP_REG_SED_EX_BOT_S = os.environ.get(
         "GROUP_REG_SED_EX_BOT_S", r"(regex|moku|BananaButler_|rgx|l4mR)bot"
     )
+    # time.py
     COUNTRY = str(os.environ.get("COUNTRY", ""))
     TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
     # For updater plugin
     UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "master")
     # dont touch this at all
     SUDO_USERS: Set[int] = set()
-    LIONUBLOGO = None
+    LIONXLOGO = None
     BOTLOG = False
     BOTLOG_CHATID = 0
+    # extra plugins realted vars below  4
+    EXTERNAL_REPOBRANCH = os.environ.get("EXTERNAL_REPOBRANCH", "main")
+    LIONX_REPO = os.environ.get("LIONX_REPO", "https://github.com/TeamLionX/LionXPlugins")
+    if LIONX_REPO and not url(LIONX_REPO):
+        LIONX_REPO = "https://github.com/TeamLionX/LionXPlugins"
+    LIONX_REPOBRANCH = os.environ.get("LIONX_REPOBRANCH", "lionx")
 
 
 class Production(Config):
